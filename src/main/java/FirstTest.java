ad1744dc4432af97548b710fe80d0876d98830a7
c0mr8 <rhgftyurkeyutghjd@gmail.com>
2023-02-20 19:25:19 +0300

    Some manipulations with timeouts and state of elements

--- src/main/java/FirstTest.java
+++ src/main/java/FirstTest.java
@@ -4,17 +4,31 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
+import java.util.concurrent.TimeUnit;
+
 public class FirstTest {
     @Test
     public void testSearchGoogle(){
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.get("http://google.com");
         WebElement searchBox = driver.findElement(By.name("q"));
+        if (searchBox.isDisplayed() == true) {
+            System.out.println("searchBox is Displayed");}
+        if (searchBox.isEnabled() == true) {
+            System.out.println("searchBox is Enabled");}
         searchBox.sendKeys("ChromeDriver");
+        searchBox.clear();
+        searchBox.click();
+        if(searchBox.isSelected() == true){
+            System.out.println("searchBox is Selected");}
+        else System.out.println("searchBox is NOT Selected");
+        searchBox.sendKeys("Java");
         searchBox.submit();
-        driver.navigate().refresh();
         driver.navigate().back();
+        driver.navigate().refresh();
+        driver.navigate().forward();
         driver.quit();
     }
 }
